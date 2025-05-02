edca81e104a28262ecad05fa9e2316cf33628d5b
Roberto C. <roberto.camacho4@gmail.com>
2025-05-02 08:17:54 +0200

    Created file dim_job_details.sql and added code

---
+++ dbt_jobads_project/models/dim/dim_job_details.sql
@@ -0,0 +1,19 @@
+WITH job_details AS (SELECT * FROM {{ ref('src_job_details') }})
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['id']) }} AS job_details_id,
+    headline,
+    description,
+    description_html,
+    COALESCE(duration, 'Ingen data') AS duration,
+    salary_type,
+    salary_description,
+    COALESCE(working_hours_type__label, 'Igen data') AS working_hours_type__label,
+    
+    CASE WHEN scope_of_work_min IS NULL THEN 'Ingen data' 
+     ELSE CAST(scope_of_work_min AS STRING) END AS scope_of_work_min,
+    
+    CASE WHEN scope_of_work_max IS NULL THEN 'Ingen data' 
+     ELSE CAST(scope_of_work_max AS STRING) END AS scope_of_work_max,
+
+FROM job_details
