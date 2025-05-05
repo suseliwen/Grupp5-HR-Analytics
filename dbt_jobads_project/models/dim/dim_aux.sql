db872f801b9ea6bf38cdadbc8f1856bdcd519b23
suseliwen <susanne.wenblad@gmail.com>
2025-05-05 07:27:30 +0200

    Added new dim_aux model including a surrogate key for unique row identification

---
+++ dbt_jobads_project/models/dim/dim_aux.sql
@@ -0,0 +1,8 @@
+with dim_aux as (select * from {{ ref('src_aux') }})
+
+select
+    {{ dbt_utils.generate_surrogate_key(['driving_license_required', 'own_car_required', 'experience_required']) }} as aux_id,
+    driving_license_required,
+    own_car_required,
+    experience_required
+from dim_aux
