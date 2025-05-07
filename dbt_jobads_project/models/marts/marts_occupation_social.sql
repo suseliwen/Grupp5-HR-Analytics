9d5cd1bab09f43b57af0dac7b8c1f29eadf6099f
suseliwen <susanne.wenblad@gmail.com>
2025-05-07 18:57:03 +0200

    Update fct_job_ads and marts structure for occupation modeling

--- dbt_jobads_project/models/marts/marts_occupation_social.sql
+++ dbt_jobads_project/models/marts/marts_occupation_social.sql
@@ -15,11 +15,9 @@ SELECT
     jd.duration,
     jd.salary_type,
     
+    
 FROM fct_job_ads f
 LEFT JOIN dim_job_details jd ON f.job_details_id = jd.job_details_id
 LEFT JOIN dim_occupation o ON f.occupation_id = o.occupation_id
+
 WHERE o.occupation_field = 'Yrken med social inriktning'
-
-
-
-
