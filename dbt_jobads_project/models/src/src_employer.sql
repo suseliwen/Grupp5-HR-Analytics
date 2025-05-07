a6430ec177ef6cc0be2b31a26a6f502ef4a02bbc
Markus Merits <markus.merits@hotmail.se>
2025-05-07 09:13:42 +0200

    Fixed name usage failures, tested src_employer [OK in 0.02s]

--- dbt_jobads_project/models/src/src_employer.sql
+++ dbt_jobads_project/models/src/src_employer.sql
@@ -3,9 +3,9 @@ SELECT id,
     employer__name as employer_name, 
     employer__workplace as employer_workplace, 
     employer__organization_number as employer_organization_number,
-    workplace_adress__street_adress as workplace_street_adress,
-    workplace__adress_region as workplace_region,
-    workplace_adress__postcode as workplace_postcode,
-    workplace_adress__city as workplace_city,
-    workplace_adress__country as workplace_country
+    workplace_address__street_address as workplace_street_address,
+    workplace_address__region as workplace_region,
+    workplace_address__postcode as workplace_postcode,
+    workplace_address__city as workplace_city,
+    workplace_address__country as workplace_country
 FROM stg_job_ads
