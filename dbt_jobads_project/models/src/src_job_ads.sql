5f09171a8947117b8df70c7b74c2b4fdf5497bba
suseliwen <susanne.wenblad@gmail.com>
2025-05-08 13:15:10 +0200

    Removed employer from the src_job_ads-model since it didn´t work

--- dbt_jobads_project/models/src/src_job_ads.sql
+++ dbt_jobads_project/models/src/src_job_ads.sql
@@ -4,9 +4,7 @@ with stg_job_ads as (select * from {{ source('job_ads', 'stg_ads') }})
 
 select
     occupation__label,
-    id,
-    employer__workplace,
-    workplace_address__municipality,
+    id,    
     number_of_vacancies as vacancies,
     relevance,
     application_deadline,
