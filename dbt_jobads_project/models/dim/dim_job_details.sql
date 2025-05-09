5abc0fc5049eedfa8c4dc70f1ce0475bcc527cd6
Roberto C. <roberto.camacho4@gmail.com>
2025-05-09 20:56:37 +0200

    Improved dim_job_details for better NULL value handling

--- dbt_jobads_project/models/dim/dim_job_details.sql
+++ dbt_jobads_project/models/dim/dim_job_details.sql
@@ -2,14 +2,15 @@ WITH job_details AS (SELECT * FROM {{ ref('src_job_details') }})
 
 SELECT
     {{ dbt_utils.generate_surrogate_key(['id']) }} AS job_details_id,
-    headline,
-    description,
-    description_html_formatted,
-    employment_type,
+    COALESCE (headline, 'Ingen data') AS headline,
+    COALESCE (description, 'Ingen data') AS description,
+    COALESCE (description_html_formatted, 'Ingen data') AS description_html_formatted,
+    COALESCE (employment_type, 'Ingen data') AS employment_type,
     COALESCE(duration, 'Ingen data') AS duration,
-    salary_type,
+    COALESCE (salary_type, 'Ingen data') AS salary_type,
     CASE WHEN scope_of_work_min IS NULL THEN 'Ingen data' 
      ELSE CAST(scope_of_work_min AS STRING) END AS scope_of_work_min,
     CASE WHEN scope_of_work_max IS NULL THEN 'Ingen data' 
      ELSE CAST(scope_of_work_max AS STRING) END AS scope_of_work_max
 FROM job_details
+WHERE id IS NOT NULL
