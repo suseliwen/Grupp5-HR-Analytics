c81624702d97215eb415dc7a2f03e6c8d8763f4d
suseliwen <susanne.wenblad@gmail.com>
2025-05-12 19:45:21 +0200

    Refactor 'jobads_dashboard' to allow user selection of a specific occupation field, ensuring the correct mart model is loaded

--- dashboard_app/jobads_dashboard.py
+++ dashboard_app/jobads_dashboard.py
@@ -11,18 +11,32 @@ from utils import DataBase_Connection
 st.set_page_config(page_title="HR Analytics Dashboard", layout="wide")
 st.title("HR Analytics Dashboard")
 
+# Conncetion between occupation field and marts
+occupation_to_mart = {
+    "Yrken med social inriktning": "marts.marts_occupation_social",
+    "Yrken med teknisk inriktning": "marts.marts_it_jobs",
+    "Chefer och verksamhetsledare" : "marts.marts_leadership_jobs"
+}
+
+# User input for occupation field
+st.sidebar.header("Välj yrkesområde")
+occupation_field_choice = st.sidebar.selectbox("Yrkesområde", list(occupation_to_mart.keys()))
+
+# Get the corresponding mart name from dictionary
+mart_table = occupation_to_mart[occupation_field_choice] 
+
 #Open data with context manager
 with DataBase_Connection() as conn:
-    df = conn.execute("SELECT * FROM marts.marts_occupation_social").fetchdf()
-    print(df)
+    df = conn.execute(f"SELECT * FROM {mart_table}").fetchdf()
+
+st.subheader(f"Visar jobbannonser för området {occupation_field_choice}")
+st.dataframe(df)
 
 # Display the DataFrame in Streamlit
-st.subheader("Visar jobbannonser för området yrken med social inriktning")
-st.dataframe(df)
 st.markdown("Här visas data baserat på användarens filtrering i sidomenyn.")
 
 # Create a sidebar for user input
-st.sidebar.header("Filtrera ditt urval:")
+st.sidebar.header("Filtrera ditt urval")
 
 # Add a selectbox for region selection
 region = df['workplace_region'].dropna().unique()
