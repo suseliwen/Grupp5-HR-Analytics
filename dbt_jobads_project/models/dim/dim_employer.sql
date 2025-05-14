979c4877e7ee498db3456c71b1461c6a44e9a014
Markus Merits <markus.merits@hotmail.se>
2025-05-14 10:43:39 +0200

    fixed name issue for employer_name

--- dbt_jobads_project/models/dim/dim_employer.sql
+++ dbt_jobads_project/models/dim/dim_employer.sql
@@ -1,6 +1,6 @@
 WITH dim_employer AS (SELECT * FROM {{ ref('src_employer') }})
 SELECT {{ dbt_utils.generate_surrogate_key(['employer_name', 'employer_workplace']) }} AS employer_id, 
-    COALESCE (employer_name, 'Ingen data') AS employer__name,
+    COALESCE (employer_name, 'Ingen data') AS employer_name,
     COALESCE (employer_workplace, 'Ingen data') AS employer_workplace,
     COALESCE (employer_organization_number, 'Ingen data') AS employer_organization_number,
     COALESCE (workplace_street_address, 'Ingen data') AS workplace_street_address,
@@ -8,4 +8,4 @@ SELECT {{ dbt_utils.generate_surrogate_key(['employer_name', 'employer_workplace
     COALESCE (workplace_postcode, 'Ingen data') AS workplace_postcode,
     COALESCE (workplace_city, 'Ingen data') AS workplace_city,
     COALESCE (workplace_country, 'Ingen data') AS workplace_country
-FROM dim_employer
+FROM dim_employer WHERE employer_id IS NOT NULL
